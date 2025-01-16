b396bb26607e67248a85106dea463bfd56ba77a3
akshmohan <145959795+akshmohan@users.noreply.github.com>
2025-01-16 11:10:26 +0530

    Initial commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:widget_testing2/user_model.dart';
+import 'package:widget_testing2/user_repository.dart';
+
+
+class MyHomePage extends StatefulWidget {
+
+
+  final Future<List<User>> futureUsers;
+
+
+  const MyHomePage({super.key, required this.title, required this.futureUsers});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final UserRepository userRepository = UserRepository();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: FutureBuilder(
+          future: widget.futureUsers,
+          builder: (context, snapshot) {
+            if(snapshot.hasData) {
+              final List<User> users = snapshot.data!;
+
+              return ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (context,index) {
+                  final user = users[index];
+                    return ListTile(
+                      title: Text(user.name),
+                      subtitle: Text(user.email),
+                    );
+              });
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            } return const CircularProgressIndicator();
+          },
+        ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:widget_testing2/home_screen.dart';
+import 'package:widget_testing2/user_repository.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home:  MyHomePage(futureUsers: UserRepository().fetchUsers(), title: 'Flutter Demo Home Page',),
+    );
+  }
+}
+
